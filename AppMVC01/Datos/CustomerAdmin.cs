fdab4e4ed4c47c129314b2606c438933e6e875dd
AREMI <iremapmedina@gmail.com>
2021-10-13 16:48:30 -0500

    Experiencia 3 de la practica de laboratorio 7 donde el objetivo es crear un CRUD

---
+++ AppMVC01/Datos/CustomerAdmin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppMVC01.Models;
+namespace AppMVC01.Datos
+{
+    public class CustomerAdmin
+    {
+        public IEnumerable<CLIENTES> Consultar()
+        {
+            using (VENTASEntities contexto = new VENTASEntities())
+            {
+                return contexto.CLIENTES.AsNoTracking().ToList();
+            }
+        }
+        public CLIENTES Consultar(int id)
+        {
+            using (VENTASEntities contexto = new VENTASEntities())
+            {
+                return contexto.CLIENTES.AsNoTracking().FirstOrDefault(c => Convert.ToInt32(c.CódigoCliente) == id);
+            }
+        }
+        public void Guardar(CLIENTES modelo)
+        {
+            using (VENTASEntities contexto = new VENTASEntities())
+            {
+                contexto.CLIENTES.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+        public void Modificar(CLIENTES modelo)
+        {
+            using (VENTASEntities contexto = new VENTASEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+        public void Eliminar(CLIENTES modelo)
+        {
+            using (VENTASEntities contexto = new VENTASEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
